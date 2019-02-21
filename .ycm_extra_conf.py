4396a39f29bbacbbe1e3b6f86b2ea88514cd65cd
Mohamed K. Eid <mohamedkeid@gmail.com>
2019-02-21 17:18:11 -0600

    Add YouCompleteMe vim plugin config

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,17 @@
+def FlagsForFile(filename, **kwargs):
+    return {
+        'flags': [
+            '-I', '/usr/local/include', 
+            '-I', '/usr/local/include/gtest',
+            '-isystem',
+            '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1',
+            '-isystem',
+            '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/include',
+            '-isystem',
+            '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+            '-isystem',
+            '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/usr/include'
+        ],
+        'do_cache':
+        False
+    }
